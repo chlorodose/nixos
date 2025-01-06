cababf43f1a813b88bfa7ed9a160f44c5f0162dd
Chlorodose <chlorodose@chlorodose.me>
2025-01-06 13:44:21 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "My configuration for nixos";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs }@inputs: { };
+}
