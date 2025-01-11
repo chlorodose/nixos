4581946a7e11ba11922edd2cfd4940e69a5df8d3
chlorodose <chlorodose@chlorodose.me>
2025-01-11 14:25:27 +0800

    feat: home-manager backup

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.extraSpecialArgs = specialArgs;
             home-manager.users.chlorodose = import ./users/chlorodose;
             home-manager.users.ling = import ./users/ling;
@@ -73,6 +74,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.extraSpecialArgs = specialArgs;
             home-manager.users.chlorodose = import ./users/chlorodose;
           }
