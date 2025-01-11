2666313ef32a558adad673ba194cc87a95ec7cb9
chlorodose <chlorodose@chlorodose.me>
2025-01-11 20:46:24 +0800

    feat: add locale settings

--- modules/basic.nix
+++ modules/basic.nix
@@ -7,6 +7,14 @@
     gnupg
   ];
 
+  i18n = {
+    defaultLocale = "C.UTF-8";
+    supportedLocales = [
+      "C.UTF-8/UTF-8"
+      "zh_CN.UTF-8/UTF-8"
+    ];
+  };
+
   time.timeZone = "Asia/Shanghai";
 
   nix.settings.substituters = lib.mkForce [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
--- users/modules/basic-home.nix
+++ users/modules/basic-home.nix
@@ -1,4 +1,7 @@
 { config, lib, pkgs, ... }: {
   programs.home-manager.enable = true;
+
+  home.language.base = "zh_CN.UTF-8";
+
   home.stateVersion = "24.11";
 }
