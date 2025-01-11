f0fd8d6299972d957edb72c9b4180d7fdd51c4d4
chlorodose <chlorodose@chlorodose.me>
2025-01-11 18:43:10 +0800

    refactor: move hyprland config to config file

--- users/chlorodose/desktop/hyprland/default.nix
+++ users/chlorodose/desktop/hyprland/default.nix
@@ -1,27 +1,11 @@
-{ config, lib, pkgs, isServer, ... }: {
+{ config, lib, pkgs, ... }: {
   wayland.windowManager.hyprland = {
     enable = true;
     systemd.enable = false; # for UWSM
     xwayland.enable = true;
   };
-
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind = [
-      "$mod, space, exec, uwsm app -- kitty"
-      "$mod, e, exec, uwsm app -- codium"
-      "$mod, b, exec, uwsm app -- firefox"
-
-
-      "$mod SHIFT, space, togglefloating,"
-      "$mod SHIFT, f, fullscreen,"
-
-      "$mod CTRL, e, exit,"
-    ];
-    bindm = [
-      "$mod, mouse:272, movewindow"
-      "$mod ALT, mouse:272, resizewindow"
-    ];
-    decoration.blur.enabled = false;
+  home.file.".config/hypr" = {
+    source = ./config;
+    recursive = true;
   };
 }
