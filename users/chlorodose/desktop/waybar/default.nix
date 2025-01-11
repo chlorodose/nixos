53e17349f8c08f4d5320f6ec41a7f3d87e946fcd
chlorodose <chlorodose@chlorodose.me>
2025-01-11 21:01:56 +0800

    feat: make waybar config

--- users/chlorodose/desktop/waybar/default.nix
+++ users/chlorodose/desktop/waybar/default.nix
@@ -1,4 +1,8 @@
 { config, lib, pkgs, ... }: {
   programs.waybar.enable = true;
   programs.waybar.systemd.enable = true;
+  home.file.".config/waybar" = {
+    source = ./config;
+    recursive = true;
+  };
 }
