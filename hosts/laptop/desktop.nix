96bb755213633be050954432612b5a5ae033ab75
chlorodose <chlorodose@chlorodose.me>
2025-01-16 23:15:18 +0800

    feat: add sddm theme

--- hosts/laptop/desktop.nix
+++ hosts/laptop/desktop.nix
@@ -2,6 +2,8 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "catppuccin-mocha";
+    package = pkgs.kdePackages.sddm;
   };
   services.displayManager.defaultSession = "hyprland-uwsm";
 
@@ -18,5 +20,10 @@
   };
   security.rtkit.enable = true;
 
-  environment.systemPackages = [ pkgs.hyprpolkitagent ];
+  environment.systemPackages = [
+    (pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+    })
+    pkgs.hyprpolkitagent
+  ];
 }
