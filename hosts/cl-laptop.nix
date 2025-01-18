eaf73b61f113c647839d7741a92588bd88bb7a62
chlorodose <chlorodose@chlorodose.me>
2025-01-19 05:21:08 +0800

    feat: add hardware config

--- hosts/cl-laptop.nix
+++ hosts/cl-laptop.nix
@@ -1,3 +1,29 @@
+{...}:
 {
   host.isDesktop = true;
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/7598fba8-77cd-4567-84e2-7749e0dced5c";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/FF5D-B7B4";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [{ device = "/dev/disk/by-uuid/ce7ab5ac-18c2-47c6-a967-040d948f38d8"; }];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = true;
+
 }
--- hosts/cl-server.nix
+++ hosts/cl-server.nix
@@ -1,7 +1,39 @@
+{lib, ...}:
+with lib;
 {
+  host.isDesktop = false;
   host.services = {
     ddns.enable = true;
     ups.enable = true;
     mihomo.enable = true; # TODO: Replace mihomo to other proxy service
   };
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "nvme" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/f897c054-be68-423a-b558-089cc3d5e302";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/5AFD-4369";
+      fsType = "vfat";
+      options = [ "rw" "sync" "nosuid" "nodev" "noexec" "uid=0" "gid=0" "umask=077" "utf8" "errors=remount-ro" ];
+    };
+
+  fileSystems."/mnt" =
+    {
+      device = "/dev/disk/by-uuid/e0837679-202c-4922-925e-40abd604b510";
+      fsType = "bcachefs";
+    };
+  systemd.extraConfig = mkForce "DefaultDeviceTimeoutSec = 3s";
+  swapDevices = [{ device = "/dev/disk/by-uuid/126e2555-a716-46e1-81a1-d065f29c6084"; }];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = true;
 }
