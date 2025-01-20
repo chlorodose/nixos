f657adfb17384c20e519d0ff3bc68c197a27f7cb
chlorodose <chlorodose@chlorodose.me>
2025-01-20 14:18:38 +0800

    feat: nixify fcitx5 setting

--- hmModules/desktop/fcitx5.nix
+++ hmModules/desktop/fcitx5.nix
@@ -1,9 +1,46 @@
 { pkgs, ... }: {
-    i18n.inputMethod = {
+  i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-gtk
       fcitx5-chinese-addons
     ];
+  };
+  xdg.configFile = {
+    "fcitx5/profile".text = ''
+      [Groups/0]
+      Name=Default
+      Default Layout=us
+      DefaultIM=keyboard-us
+
+      [Groups/0/Items/0]
+      Name=keyboard-us
+      Layout=
+
+      [Groups/0/Items/1]
+      Name=pinyin
+      Layout=
+
+      [GroupOrder]
+      0=Default
+    '';
+    "fcitx5/config".text = ''
+      [Hotkey]
+      EnumerateWithTriggerKeys=True
+
+      [Hotkey/TriggerKeys]
+      0=Shift+Shift_L
+      1=Shift+Shift_R
+
+      [Hotkey/PrevPage]
+      0=Up
+
+      [Hotkey/NextPage]
+      0=Down
+    '';
+    "fcitx5/conf/cloudpinyin.conf".text = ''
+      MinimumPinyinLength=3
+      Backend=GoogleCN
+    '';
   };
 }
